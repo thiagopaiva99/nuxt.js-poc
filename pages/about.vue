666617cdf248a8f095c04dbbd21181e77824a081
Thiago Galvani <thiagopaiva99@gmail.com>
2018-03-24 19:11:12 -0300

    First pages: [index, about]

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>Hi! Little app developed with <strong>Nuxt.js</strong>!</p>
+    <nuxt-link to="/">Home page</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    return {
+      
+    }
+  },
+  head: {
+    title: 'About page'
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Welcome!</h1>
+    <nuxt-link to="/about">About page</nuxt-link>
+
+    <p><a href="https://github.com/thiagopaiva99">@thiagopaiva99</a></p>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: "Home page"
+  }
+};
+</script>
